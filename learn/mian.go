47dfd2d9598297fd13d3e44969b2adf400d9b459
AndyLuffy <huangshiyang16@yeah.net>
2025-09-23 22:21:59 +0800

    个人博客系统后端

--- learn/mian.go
+++ learn/mian.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/learn/go_task/learn/task_3"
+	"github.com/gin-gonic/gin"
+	"github.com/learn/go_task/learn/task_4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,6 +10,19 @@ import (
 func main() {
 
 	dsn := "root:admin@123456@tcp(192.168.3.59:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
-	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	task_3.Insert(db)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		task_4.Register(c, db)
+	})
+
+	err2 := router.Run(":8080")
+	if err2 != nil {
+		panic(err2)
+	}
+
 }
